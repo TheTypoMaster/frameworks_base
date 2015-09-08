0f9d1b3eacf26bc9e8293b8df1f78f97458f911b
mydongistiny <jaysonedson@gmail.com>
2015-09-08 16:10:52 -0700

    SlimNavRing: Update drawable for lastapp and update colors.

--- packages/SystemUI/src/com/android/systemui/slimnavrings/NavigationRingHelpers.java
+++ packages/SystemUI/src/com/android/systemui/slimnavrings/NavigationRingHelpers.java
@@ -117,6 +117,8 @@ public class NavigationRingHelpers {
             resourceId = getRingerDrawableResId(context);
         } else if (action.equals(ACTION_KILL)) {
             resourceId = R.drawable.ic_navigation_ring_killtask;
+        } else if (action.equals(ACTION_LAST_APP)) {
+            resourceId = R.drawable.ic_navigation_ring_lastapp;
         } else if (action.equals(ACTION_POWER)) {
             resourceId = R.drawable.ic_navigation_ring_power;
         } else if (action.equals(ACTION_TORCH)) {
